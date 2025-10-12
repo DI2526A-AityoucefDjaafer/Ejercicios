fed840eb0c26c399be79145f0b68f18a2d6600f5
Djaafer016 <djaait@alu.edu.gva.es>
2025-10-12 23:06:03 +0200

    subiendo los ejercicios

---
+++ A3.1/A3.1.3_Mosaico/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace A3._1._3_Mosaico
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ A3.1/A3.1.5_Calculadora/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace A3._1._5_Calculadora
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
