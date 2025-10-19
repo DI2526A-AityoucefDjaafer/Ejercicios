abfe1d6e96c69fea8c6611a2f8b566c0886a52c4
Djaafer016 <djaait@alu.edu.gva.es>
2025-10-19 17:09:05 +0200

    subiendo ejercicios estilos

---
+++ A3.2/A3.2_AitYoucefDjaafer_Estilos/A3.2.3_VisorImagenes_Estilo/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace A3._2._3_VisorImagenes_Estilo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ A3.2/A3.2_AitYoucefDjaafer_Estilos/A3.2.6_Calculadora_Dinamica/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+
+namespace A3._2._4_Calculadora_Estilos
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CrearBotones();
+        }
+
+        private void CrearBotones()
+        {
+            for (int i = 0; i < 4; i++)
+                gridBotones.RowDefinitions.Add(new RowDefinition());
+            for (int j = 0; j < 3; j++)
+                gridBotones.ColumnDefinitions.Add(new ColumnDefinition());
+
+            var estiloBoton = (Style)this.FindResource("EstiloBoton");
+
+            int numero = 1;
+            for (int fila = 0; fila < 3; fila++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    Button boton = new Button();
+                    boton.Content = numero.ToString();
+                    boton.Style = estiloBoton;
+                    boton.Click += Button_Click;
+
+                    Grid.SetRow(boton, fila);
+                    Grid.SetColumn(boton, col);
+                    gridBotones.Children.Add(boton);
+
+                    numero++;
+                }
+            }
+
+            Button boton0 = new Button();
+            boton0.Content = "0";
+            boton0.Style = estiloBoton;
+            boton0.Click += Button_Click;
+
+            Grid.SetRow(boton0, 3);
+            Grid.SetColumn(boton0, 0);
+            Grid.SetColumnSpan(boton0, 3);
+
+            gridBotones.Children.Add(boton0);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button boton = (Button)sender;
+            txtDisplay.Text += boton.Content.ToString();
+        }
+    }
+}
